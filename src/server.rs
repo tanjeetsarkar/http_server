f73931d03a03c0e4f12ba15253aaee9bf4a23164
Tanjeet Sarkar <tjspideydon@gmail.com>
2023-03-19 15:31:55 +0530

    Initial commit

---
+++ src/server.rs
@@ -0,0 +1,56 @@
+use crate::http::{ParseError, Request, Response, StatusCode};
+use std::convert::TryFrom;
+use std::{io::Read, net::TcpListener};
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse Request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send Response: {}", e);
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {e}"),
+                    }
+                }
+                Err(e) => println!("failed to establish connection: {}", e),
+            }
+
+            // let res = listener.accept();
+
+            // if res.is_err() {
+            //     continue;
+            // }
+
+            // let (stream, addr) = res.unwrap();
+        }
+    }
+}
